9b7f259bba1840a811ceb124cf67f34727eeb249
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-04-28 17:24:36 +0200

    committing tests and endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,11 +39,44 @@ describe("GET /api/topics", () => {
         });
       });
   });
-  test(
-    "404: Responds with an error when attempting to access a non existent endpoint", () => {
-      return request(app)
-    .get("/api/ninenine")
-    .expect(404)
-    });
-    });
+  test("404: Responds with an error when attempting to access a non existent endpoint", () => {
+    return request(app).get("/api/ninenine").expect(404);
+  });
+});
 
+describe("GET /api/articles/:article_id", () => {
+  test("200: Gets an article by its ID and responds with an article object containing the specified properties below", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "some gifs",
+          created_at: "2020-11-03T08:12:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("400: Resonds with a 400 error when passed bad request", () => {
+    return request(app)
+      .get("/api/articles/beatle")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("400: Bad request");
+      });
+  });
+  test("404: Responds with a 404 error when given valid request, but no data exists", () => {
+    return request(app)
+      .get("/api/articles/30")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: Error");
+      });
+  });
+});
